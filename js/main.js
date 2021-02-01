132e3b2882eee00285a2919b288f009e637e323b
Aivarelio <caosas21@gmail.com>
2021-02-01 14:25:35 +0200

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log('Coming soon...');
+import { clock } from './components/clock.js';
+import { socialsData } from './data/socialsData.js';
+import { renderSocials } from './components/renderSocials.js';
+
+clock('.clock', '01-04 14:00:00');
+renderSocials('footer > .socials', socialsData);
